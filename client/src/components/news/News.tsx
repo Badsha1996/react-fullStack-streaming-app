1c4dba9d4f3a0ae3d5e5ae3f917fa1d8b6b70399
Badsha Laskar <43995221+Badsha1996@users.noreply.github.com>
2022-12-18 14:56:41 +0530

    third commit

--- client/src/components/news/News.tsx
+++ client/src/components/news/News.tsx
@@ -1,5 +1,6 @@
-import { YouTube } from "@material-ui/icons";
 import "./news.scss"
+import TrailerButton from "../trailer-button/TrailerButton";
+import { AcUnit, LiveTv, People } from "@material-ui/icons";
 
 
 const News = ({animeData}:{animeData : any}) => { 
@@ -14,18 +15,22 @@ const News = ({animeData}:{animeData : any}) => {
                     
                     return (
                         <div className="container" key={index}>
-                        
+                            
+{/*                                 
+                                <People className="people-icon"/>{anime.members}
+                                 <LiveTv className="tv-icon"/>{(anime.studios.length >= 1) ? anime.studios.name : ""}
+                                <AcUnit className="season-icon"/>{anime.season} */}
+                            
                             <span>
                                 <div className="play-icon" >
-                                
-                                  <YouTube className="icon" />
+                                  <TrailerButton animeURL = {anime.trailer.embed_url}/>
                                 </div>
-
                             </span>
                             <div className="container__card">
                                 <img src={anime.images.jpg.large_image_url} alt="anime-poster" />
                             </div>
-                            <p>{anime.title.slice(0,50)}</p>
+                            {/* <span className="anime-genre">{anime.genres[0].name}</span> */}
+                            <p className="ani-title">{anime.title.slice(0,50)}</p>
                         </div>
                     )
                 })
---
+++ client/src/components/trailer-button/TrailerButton.tsx
@@ -0,0 +1,41 @@
+import { CloseOutlined, YouTube } from "@material-ui/icons"
+import { useState } from "react"
+import "./trailerButton.scss"
+
+
+
+
+
+const TrailerButton = ({animeURL}:{animeURL : any}) => {
+    const [isClicked, setIsClicked] = useState(false)
+   const  handlePlay = () =>{
+        setIsClicked(true)
+   }
+   const handleClose = () =>{
+        setIsClicked(false)
+   }
+  return (
+    <div className="TrailerButton">
+        <YouTube className="icon" onClick={handlePlay}/>
+        {
+            isClicked && (
+                <>
+                    <div className="video-responsive">
+                        <CloseOutlined onClick={handleClose} className="close-icon"/>
+                        <iframe
+                        width="360"
+                        height="360"
+                        src={animeURL}
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        title="Embedded youtube"
+                        />
+                </div>
+                </>
+            )
+        }
+    </div>
+  )
+}
+
+export default TrailerButton
