70f67b25ef7d89605a1d457a7da4aeb5cc565cb0
Badsha Laskar <43995221+Badsha1996@users.noreply.github.com>
2022-12-24 09:35:59 +0530

    eleventh commit

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS =  require("crypto-js");
 
 // REGISTER USER FROM SIGN UP
 router.post("/register", async(req, res) => {
     const newUser =  new User({
         username : req.body.username,
         email : req.body.email,
-        password : req.body.password
+        password : CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString()
     })
 
     try {
@@ -17,6 +18,26 @@ router.post("/register", async(req, res) => {
         res.status(500).json(err);
     }
     
+})
+
+// LOG IN USER FROM SIGN IN
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({email : req.body.email});
+        !user && res.status(401).json("Not found!")
+
+        const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const originalPassword =  bytes.toString(CryptoJS.enc.Utf8);
+
+        if(originalPassword !== req.body.password){
+            res.status(401).json("wrong password")
+        }else{
+            const {password, ...info} = user._doc;
+            res.status(201).json(info)
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
 })
 
 module.exports = router;
