2ea9f77295702aac63510c28c671a73ca7bd49f0
Badsha Laskar <43995221+Badsha1996@users.noreply.github.com>
2022-12-24 11:45:12 +0530

    thirtheen commit

--- server/routes/users.js
+++ server/routes/users.js
@@ -10,7 +10,8 @@ router.put("/:id", verify ,async(req, res)=>{
             req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString()
         }
         try{
-            const updatedUser = await User.findByIdAndUpdate(req.params.id, {$set : req.body});
+            const updatedUser = await User.findByIdAndUpdate(req.params.id, {$set : req.body},
+            {new : true});
             res.status(201).json(updatedUser)
         }catch(err){
             res.status(500).json(err)
@@ -20,4 +21,44 @@ router.put("/:id", verify ,async(req, res)=>{
     }
 })
 
+// DELETE METHOD
+router.delete("/:id", verify ,async(req, res)=>{
+    if(req.user.id === req.params.id || req.user.isAdmin){
+        try{
+           await User.findByIdAndDelete(req.params.id)
+           res.status(201).json("User Deleted")
+        }catch(err){
+            res.status(500).json(err)
+        }
+    }else{
+        res.status(403).json("You are not allowed to delete")
+    }
+})
+
+// FIND USER 
+router.get("/find/:id", verify ,async(req, res)=>{
+        try{
+            const user = await User.findById(req.params.id);
+            const {password, ...info} = user._doc;
+            res.status(201).json(info)
+        }catch(err){
+            res.status(500).json(err)
+        }
+    
+})
+
+// FIND ALL USER
+router.get("/", verify ,async(req, res)=>{
+    const query = req.query.new;
+    if(req.user.isAdmin){
+        try{
+           const users = query ? await User.find().limit(10) : await User.find();
+           res.status(201).json(users)
+        }catch(err){
+            res.status(500).json(err)
+        }
+    }else{
+        res.status(403).json("Only Admins are allowed")
+    }
+})
 module.exports = router;
